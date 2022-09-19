3fb14c2fd7e4f20277d5db65af923c91696e6aa0
BasharAl-Mohammad <bash.mh.99@gmail.com>
2022-09-19 10:20:33 +0300

    Added GetCandyUser trait to User Model/ Relationships establishement

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use GetCandy\Base\Traits\GetCandyUser;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, GetCandyUser;
 
     /**
      * The attributes that are mass assignable.
